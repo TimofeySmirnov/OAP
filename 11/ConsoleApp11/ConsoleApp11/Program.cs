aa80fe7131d243b9d51cecd84ee027ea300335c6
TimofeySmirnov <115399337+TimofeySmirnov@users.noreply.github.com>
2023-09-15 01:47:43 +0300

    Add files via upload

---
+++ 11/ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите длину окружности:");
+        double circumference = Convert.ToDouble(Console.ReadLine());
+
+        double radius = circumference / (2 * Math.PI);
+        double area = (Math.PI * Math.Pow(radius, 2));
+
+        Console.WriteLine($"Площадь круга: {area}");
+    }
+}
